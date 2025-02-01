c4449d2edf013c0d3f3248c45ea3c9365b8062e2
Nate Allen <nateonmission@gmail.com>
2025-02-01 09:13:48 -0600

    optimized day 3

--- 3/a.py
+++ 3/a.py
@@ -1,28 +1,29 @@
 import re
 
+
+
 # Define the regex pattern
-pattern = r"mul\(\d{1,3},\d{1,3}\)|don't\(\)|do\(\)"
-pattern2 = "don't\(\)|do\(\)"
+pattern = re.compile(r"mul\(\d{1,3},\d{1,3}\)|don't\(\)|do\(\)")
+collector = 0
+skip = False
 
 # Initialize a list to store matches
 matches = []
 with open("data.txt", "r") as file:
     for line in file:
-        matches.extend(re.findall(pattern, line)) 
-        
-collector = 0
-skip = False
-for pair in matches:
-    # print(f"{pair[4:pair.index(',')]} : {pair[pair.index(',')+1 : -1]}")
-    if "do()" in pair:
-        skip = False
-    elif "don't()" in pair:
-        skip = True
-    print(f"{skip = }, {pair}")
-    if not skip and "mul" in pair:
-        a = int(pair[4:pair.index(',')])
-        b = int(pair[pair.index(',')+1 : -1])
-        collector += a*b
+        for match in pattern.finditer(line):
+            pair = match.group() 
+            # print(f"{pair[4:pair.index(',')]} : {pair[pair.index(',')+1 : -1]}")
+            if "do()" in pair:
+                skip = False
+            elif "don't()" in pair:
+                skip = True
+            elif not skip and pair.startswith("mul"):
+                start = pair.index('(') + 1
+                mid = pair.index(',')
+                end = pair.index(')')
+                a, b = int(pair[start:mid]), int(pair[mid+1:end])
+                collector += a*b
         
     
 print(f"{collector = }")
